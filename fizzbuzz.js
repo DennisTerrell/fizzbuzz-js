6b60d466fd28b9b984b6c61a13e258c9ad2f59d6
Dennis <dennisterrell@gmail.com>
2016-08-04 16:10:42 -0700

    Initial commit

---
+++ fizzbuzz.js
@@ -0,0 +1,18 @@
+for (var i=1; i<= 100; i++){ 
+
+  var divideByThree = i % 3 === 0;
+  var divdieByFive = i % 5 === 0;
+
+  if (divideByThree && dividebyFive){
+    console.log('FizzBuzz');
+  }
+  else if (divideByThree){
+    console.log('Fizz');
+  }
+  else if (divideByFive){
+    console.log('Buzz')
+  }
+  else{
+    console.log(i);
+  }
+}
