404700a8431c8118dbfbf86ba973af9a3b8255c8
Dennis <dennisterrell@gmail.com>
2016-08-04 16:37:56 -0700

    fixed bad syntax and typos

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,16 +1,16 @@
 for (var i=1; i<= 100; i++){ 
 
   var divideByThree = i % 3 === 0;
-  var divdieByFive = i % 5 === 0;
+  var divideByFive = i % 5 === 0;
 
-  if (divideByThree && dividebyFive){
+  if (divideByThree && divideByFive){
     console.log('FizzBuzz');
   }
   else if (divideByThree){
     console.log('Fizz');
   }
   else if (divideByFive){
-    console.log('Buzz')
+    console.log('Buzz');
   }
   else{
     console.log(i);
